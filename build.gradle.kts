4536c5f1d754e916ab87722b3845a2dfac843b56
Tyler Cook <tcc@sandpolis.com>
2021-06-13 19:17:04 -0500

    Update buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,20 +17,20 @@ plugins {
 }
 
 dependencies {
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.1")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.1")
-	testImplementation("org.awaitility:awaitility:4.0.1")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+	testImplementation("org.awaitility:awaitility:4.1.0")
 
 	// https://github.com/netty/netty
-	api("io.netty:netty-common:4.1.60.Final")
-	api("io.netty:netty-codec:4.1.60.Final")
-	api("io.netty:netty-codec-dns:4.1.60.Final")
-	api("io.netty:netty-transport:4.1.60.Final")
-	api("io.netty:netty-handler:4.1.60.Final")
-	api("io.netty:netty-resolver-dns:4.1.60.Final")
+	api("io.netty:netty-common:4.1.65.Final")
+	api("io.netty:netty-codec:4.1.65.Final")
+	api("io.netty:netty-codec-dns:4.1.65.Final")
+	api("io.netty:netty-transport:4.1.65.Final")
+	api("io.netty:netty-handler:4.1.65.Final")
+	api("io.netty:netty-resolver-dns:4.1.65.Final")
 
 	if (project.getParent() == null) {
-		api("com.sandpolis:core.instance:0.1.0")
+		api("com.sandpolis:core.instance:0.2.0")
 	} else {
 		api(project(":module:com.sandpolis.core.instance"))
 	}
